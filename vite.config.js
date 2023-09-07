916a278f7a76235640c70fd2e6498cfbd79ee143
TanLiangJin <liangjin0228@gmail.com>
2023-09-08 00:43:24 +0800

    second time try to fix github pages problem

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
-const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
 const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
@@ -47,8 +48,9 @@ export default defineConfig({
         // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
         sanitizeFileName(name) {
           const match = DRIVE_LETTER_REGEX.exec(name);
-          const driveLetter = match ? match[0] : '';
-          // substr 是被淘汰語法，因此要改 slice
+          const driveLetter = match ? match[0] : "";
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
           return (
             driveLetter +
             name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
