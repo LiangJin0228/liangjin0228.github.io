31fa2866dec0370953ef26f1e24f4631d2af5b0a
TanLiangJin <liangjin0228@gmail.com>
2023-09-20 21:53:06 +0800

    cancel hide-scroll-bar style scoped, make resume page

--- src/component/HomeCard.vue
+++ src/component/HomeCard.vue
@@ -66,18 +66,3 @@ export default {
         </v-card>
     </v-dialog>
 </template>
-
-<style scoped>
-/* Hide scrollbar for Chrome, Safari and Opera */
-.hide-scrollbar::-webkit-scrollbar {
-    display: none;
-}
-
-/* Hide scrollbar for IE, Edge and Firefox */
-.hide-scrollbar {
-    -ms-overflow-style: none;
-    /* IE and Edge */
-    scrollbar-width: none;
-    /* Firefox */
-}
-</style>
