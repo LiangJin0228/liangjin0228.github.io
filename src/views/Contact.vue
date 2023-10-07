6c47653419e5da6df8052b65cd4ac07de21c7115
TanLiangJin <liangjin0228@gmail.com>
2023-10-08 00:18:00 +0800

    complete composition

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -19,29 +19,51 @@
                 <v-btn variant="tonal" color="success">Send Mail To Liang Jin</v-btn>
             </v-card-actions>
         </v-card>
-        <v-card class="ma-7 pa-3">
-            <v-card-title>
-                <v-icon>mdi-github</v-icon> GitHub
-            </v-card-title>
 
-        </v-card>
-        <v-card class="ma-7 pa-3">
-            <v-card-title>
-                <v-icon>mdi-linkedin</v-icon> Linkedin
-            </v-card-title>
+        <v-row>
+            <v-col>
+                <v-card class="ma-7 pa-3" target="_blank" href="https://github.com/LiangJin0228">
+                    <v-card-title>
+                        <v-icon>mdi-github</v-icon> GitHub
+                    </v-card-title>
+                    <v-card-subtitle>
+                        我會將我的Side Project放在Github上,有興趣的歡迎貢獻
+                    </v-card-subtitle>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card class="ma-7 pa-3" target="_blank" href="https://www.linkedin.com/in/liangjin0228/">
+                    <v-card-title>
+                        <v-icon>mdi-linkedin</v-icon> Linkedin
+                    </v-card-title>
+                    <v-card-subtitle>
+                        歡迎追蹤 !
+                    </v-card-subtitle>
+                </v-card>
+            </v-col>
+        </v-row>
 
-        </v-card>
-        <v-card class="ma-7 pa-3">
-            <v-card-title>
-                <v-icon>mdi-file-account</v-icon> 104
-            </v-card-title>
-
-        </v-card>
-        <v-card class="ma-7 pa-3">
-            <v-card-title>
-                <v-icon>mdi-web</v-icon> ithome
-            </v-card-title>
-
-        </v-card>
+        <v-row>
+            <v-col>
+                <v-card class="ma-7 pa-3" target="_blank">
+                    <v-card-title>
+                        <v-icon>mdi-file-account</v-icon> 104
+                    </v-card-title>
+                    <v-card-subtitle>
+                        暫不開放
+                    </v-card-subtitle>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card class="ma-7 pa-3" target="_blank" href="https://ithelp.ithome.com.tw/users/20162542">
+                    <v-card-title>
+                        <v-icon>mdi-web</v-icon> ithome
+                    </v-card-title>
+                    <v-card-subtitle>
+                        不定期在這邊發表一些技術文章
+                    </v-card-subtitle>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-contaner>
 </template>
