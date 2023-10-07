5d0aee5fdb5b6d08d6157cbe34ad787e3d7c089f
TanLiangJin <liangjin0228@gmail.com>
2023-10-07 23:20:42 +0800

    init comment of  branch contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,1 +1,28 @@
-<template></template>
+<template>
+    <v-contaner>
+        <v-card>
+            <v-card-item>
+                <v-form>
+                    <v-row>
+                        <v-col>
+                            <v-text-field variant="outlined" label="Name" prepend-icon="mdi-account"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field variant="outlined" label="Email" prepend-icon="mdi-email"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-text-field variant="outlined" label="Subject"></v-text-field>
+                    <v-textarea variant="outlined" label="Message..."></v-textarea>
+                </v-form>
+            </v-card-item>
+            <v-card-actions>
+                <v-btn variant="tonal" color="success">Send Mail To Liang Jin</v-btn>
+            </v-card-actions>
+        </v-card>
+        <v-card></v-card>
+        <v-card></v-card>
+        <v-card></v-card>
+        <v-card></v-card>
+
+    </v-contaner>
+</template>
