ead5c33271dc76576e4960dd0f8c364cdb6541ae
TanLiangJin <liangjin0228@gmail.com>
2023-10-10 14:06:40 +0800

    RWD-header changed

--- src/layouts/header_footer/Header.vue
+++ src/layouts/header_footer/Header.vue
@@ -8,66 +8,59 @@ const route = useRoute();
 const path = computed(() => route.path.replace("/", ""));
 const pageTitle = ref("");
 
-const { mobile } = useDisplay();
+const { smAndDown } = useDisplay();
 
 onMounted(() => {
   pageTitle.value = path.value;
 });
 
 const buttons = [
-    {
-        value: 'index',
-        text: 'Home',
-        icon: 'mdi-home',
-        textClass: 'text-green',
-        to: '/index',
-    },
-    {
-        value: 'resume',
-        text: 'Resume',
-        icon: 'mdi-file-account',
-        textClass: 'text-brown-darken-1',
-        to: '/resume',
-    },
-    {
-        value: 'about',
-        text: 'About Me',
-        icon: 'mdi-information',
-        textClass: 'text-blue',
-        to: '/about',
-    },
-    {
-        value: 'contact',
-        text: 'Contact Me',
-        icon: 'mdi-phone-incoming',
-        textClass: 'text-yellow-darken-4',
-        to: '/contact',
-    },
+  {
+    value: 'index',
+    text: 'Home',
+    icon: 'mdi-home',
+    textClass: 'text-green',
+    to: '/index',
+  },
+  {
+    value: 'resume',
+    text: 'Resume',
+    icon: 'mdi-file-account',
+    textClass: 'text-brown-darken-1',
+    to: '/resume',
+  },
+  {
+    value: 'about',
+    text: 'About Me',
+    icon: 'mdi-information',
+    textClass: 'text-blue',
+    to: '/about',
+  },
+  {
+    value: 'contact',
+    text: 'Contact Me',
+    icon: 'mdi-phone-incoming',
+    textClass: 'text-yellow-darken-4',
+    to: '/contact',
+  },
 ];
 </script>
 
 <template>
   <v-app-bar app flat :elevation="5">
-    <v-app-bar-nav-icon v-if="!mobile">
+    <v-app-bar-nav-icon v-if="!smAndDown">
       <v-icon icon="mdi-human-greeting" color="green-darken-4" />
     </v-app-bar-nav-icon>
-    <v-app-bar-title class="text-deep-purple-lighten-2" v-if="!mobile">
+    <v-app-bar-title class="text-deep-purple-lighten-2" v-if="!smAndDown">
       Welcome!
     </v-app-bar-title>
-    <v-spacer v-if="mobile"></v-spacer>
+    <v-spacer v-if="smAndDown"></v-spacer>
 
     <v-btn-toggle v-model="pageTitle">
-      <v-btn
-        v-for="button in buttons"
-        :key="button.value"
-        rounded="xl"
-        :size="mobile ? 'x-large' : 'large'"
-        :class="button.textClass"
-        :value="button.value"
-        :to="button.to"
-      >
-        <v-icon :icon="button.icon" :size="mobile ? 'x-large' : 'large'" />
-        <span v-if="!mobile">{{ button.text }}</span>
+      <v-btn v-for="button in buttons" :key="button.value" rounded="xl" :size="smAndDown ? 'x-large' : 'large'"
+        :class="button.textClass" :value="button.value" :to="button.to">
+        <v-icon :icon="button.icon" :size="smAndDown ? 'x-large' : 'large'" />
+        <span v-if="!smAndDown">{{ button.text }}</span>
       </v-btn>
     </v-btn-toggle>
     <v-spacer></v-spacer>
